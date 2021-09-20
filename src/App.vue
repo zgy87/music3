c8af7b16d3a950f905ea39b80b8e41ad3d685c45
zgy919 <2625862106@qq.com>
2021-09-20 11:30:16 +0800

    919下班

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <div id="app">
-    <div class="test"></div>
-    <div>段落段落</div>
+    <Header></Header>
+    <Tabbar></Tabbar>
   </div>
 </template>
-
-<style lang="scss">
-  .test{
-    width: 100px;
-    height: 100px;
-    background: #d43c33;
-    //transform: translate(100px,100px);
+<script>
+import Header from './components/Header'
+import Tabbar from './components/Tabbar'
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Header,
+    // eslint-disable-next-line vue/no-unused-components
+    Tabbar
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import "assets/css/variable";
+  .theme{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    button{
+      width: 100px;
+      height: 100px;
+      &:nth-of-type(1){
+        background: $background-color-theme;
+      }
+      &:nth-of-type(2){
+        background: $background-color-theme1;
+      }
+      &:nth-of-type(3){
+        background: $background-color-theme2;
+      }
+    }
   }
 </style>
