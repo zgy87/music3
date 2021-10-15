d77c7d20f6f6e263336791fc4c89fa9c7ae28b30
zgy919 <2625862106@qq.com>
2021-10-15 17:07:20 +0800

    170

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -7,19 +7,37 @@
     <div class="search-suggest" v-show="keywords !== ''">
       <ScrollView>
         <ul>
-          <li v-for="value in songs" :key="value.id">
+          <li v-for="value in songs" :key="value.id" @click.stop="selectMusic(value.id)">
             <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNiAyNiI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBmaWxsPSIjYzljOWNhIiBkPSJNMjUuMTgxIDIzLjUzNWwtMS40MTQgMS40MTQtNy4zMTUtNy4zMTRBOS45NjYgOS45NjYgMCAwIDEgMTAgMjBDNC40NzcgMjAgMCAxNS41MjMgMCAxMFM0LjQ3NyAwIDEwIDBzMTAgNC40NzcgMTAgMTBjMCAyLjM0Mi0uODExIDQuNDktMi4xNiA2LjE5NWw3LjM0MSA3LjM0ek0xMCAyYTggOCAwIDEgMCAwIDE2IDggOCAwIDAgMCAwLTE2eiIvPjwvc3ZnPg==" alt="">
             <p>{{ value.name }} - {{ value.artists[0].name }}</p>
           </li>
         </ul>
       </ScrollView>
     </div>
+    <div class="search-hot">
+      <h3>热门搜索</h3>
+      <ul>
+        <li v-for="value in hots" :key="value.first" @click.stop="selectHot(value.first)">{{ value.first }}</li>
+      </ul>
+    </div>
+    <ul class="search-history">
+      <li>
+        <div class="history-left">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMCAzMCI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBmaWxsPSIjYzljYWNhIiBkPSJNMTUgMzBDNi43MTYgMzAgMCAyMy4yODQgMCAxNVM2LjcxNiAwIDE1IDBzMTUgNi43MTYgMTUgMTUtNi43MTYgMTUtMTUgMTVtMC0yOEM3LjgyIDIgMiA3LjgyIDIgMTVzNS44MiAxMyAxMyAxMyAxMy01LjgyIDEzLTEzUzIyLjE4IDIgMTUgMm03IDE2aC04YTEgMSAwIDAgMS0xLTFWN2ExIDEgMCAxIDEgMiAwdjloN2ExIDEgMCAxIDEgMCAyIi8+PC9zdmc+" alt="">
+        </div>
+        <div class="history-right">
+          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBmaWxsPSIjOTk5ODk5IiBkPSJNMTMuMzc5IDEybDEwLjMzOCAxMC4zMzdhLjk3NS45NzUgMCAxIDEtMS4zNzggMS4zNzlMMTIuMDAxIDEzLjM3OCAxLjY2MyAyMy43MTZhLjk3NC45NzQgMCAxIDEtMS4zNzgtMS4zNzlMMTAuNjIzIDEyIC4yODUgMS42NjJBLjk3NC45NzQgMCAxIDEgMS42NjMuMjg0bDEwLjMzOCAxMC4zMzhMMjIuMzM5LjI4NGEuOTc0Ljk3NCAwIDEgMSAxLjM3OCAxLjM3OEwxMy4zNzkgMTIiLz48L3N2Zz4=" alt="">
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import ScrollView from '../components/ScrollView'
-import { getSearchList } from '../api/index'
+import { getSearchList, getSearchHot } from '../api/index'
+import { mapActions } from 'vuex'
+import { setLocalStorage, getLocalStorage } from '@/tools/tools'
 
 export default {
   name: 'Search',
@@ -30,10 +48,17 @@ export default {
   data () {
     return {
       keywords: '',
-      songs: []
+      songs: [],
+      hots: [],
+      searchHistory: []
     }
   },
   methods: {
+    ...mapActions([
+      'setFullScreen', // 将 `this.increment()` 映射为 `this.$store.dispatch('increment')`
+      // 'setMiniPlayer'
+      'setSongDetail'
+    ]),
     search () {
       console.log('发送网络请求')
       getSearchList({ keywords: this.keywords })
@@ -44,6 +69,21 @@ export default {
         .catch(function (err) {
           console.log(err)
         })
+    },
+    selectMusic (id) {
+      this.setFullScreen(true)
+      // this.setMiniPlayer(false)
+      this.setSongDetail([id])
+      if (this.searchHistory.includes(this.keywords)) {
+        return
+      }
+      this.searchHistory.push(this.keywords)
+      setLocalStorage('searchHistory', this.searchHistory)
+      this.keywords = ''
+    },
+    selectHot (name) {
+      this.keywords = name
+      this.search()
     }
   },
   directives: {
@@ -63,6 +103,19 @@ export default {
         })
       }
     }
+  },
+  created () {
+    getSearchHot()
+      .then((data) => {
+        this.hots = data.result.hots
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    if (getLocalStorage('searchHistory') === undefined || getLocalStorage('searchHistory') === null) {
+      return
+    }
+    this.searchHistory = getLocalStorage('searchHistory')
   }
 }
 </script>
@@ -107,6 +160,7 @@ export default {
     //background: #bb8888;
     overflow: hidden;
     //background: #ffcc33;
+    @include bg_sub_color;
     li{
       display: flex;
       align-items: center;
@@ -121,6 +175,27 @@ export default {
         margin-left: 20px;
         @include font_color;
         @include font_size($font_medium);
+      }
+    }
+  }
+  .search-hot{
+    h3{
+      @include font_color;
+      @include font_size($font_medium);
+      padding: 10px 20px;
+    }
+    ul{
+      display: flex;
+      flex-wrap: wrap;
+      li{
+        height: 60px;
+        line-height: 60px;
+        border: 1px solid #000;
+        border-radius: 30px;
+        padding: 0 20px;
+        @include font_color;
+        @include font_size($font_medium_s);
+        margin: 10px 20px;
       }
     }
   }
